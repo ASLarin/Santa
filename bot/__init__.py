bd3aa3dc3a0fd755983ff789777026d6cd9ed91f
FructorMG <fidog@mail.ru>
2024-10-14 17:03:10 -0400

    Инициализация мидлваров + хендлеров Хендлеры - добавить после добавления.

---
+++ bot/__init__.py
@@ -0,0 +1,13 @@
+from bot.bot import dp
+from bot.Middleware import RoleMiddleware, ThrottlingMiddleware
+
+from bot.Handlers import *
+from bot.Keyboards import *
+from bot.Utils import *
+from bot.Services import *
+
+def setup_middlewares():
+    dp.middleware.setup(RoleMiddleware())
+    dp.middleware.setup(ThrottlingMiddleware())
+def setup_handlers():
+    pass
